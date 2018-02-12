a54bb98ae29ccad2a9a9a9bd748a0199df6472ea
thward <thomas.h.ward@gmail.com>
2018-02-12 18:26:25 +1100

    Added client side validation

--- src/main/java/childrensgame/ChildrensGame.java
+++ src/main/java/childrensgame/ChildrensGame.java
@@ -2,11 +2,13 @@ package childrensgame;
 
 import java.util.Scanner;
 
+import org.apache.commons.lang3.StringUtils;
+
 import childrensgame.service.ChildService;
 import childrensgame.service.impl.ChildServiceImpl;
 
 /**
- * Calls service to find winner of Children's Game 
+ * Command line client to find winner of Children's Game 
  * 
  * @author Tom Ward
  * @version 1.0
@@ -17,18 +19,38 @@ public class ChildrensGame {
 		
 		ChildService childService = new ChildServiceImpl();
 		
-		// Example call to service
-		// Given a circle size of 3 and elimination count of 4, returns id of winning child 1 
 		boolean carryOn = true;
 		Scanner scanner = new Scanner(System.in);
 		
 		while (carryOn){
 						
-			System.out.print("Enter circle size : ");
-			Integer circleSize = Integer.parseInt(scanner.nextLine());
+			String inputChar = null;
 			
-			System.out.print("Enter elimination count : ");
-			Integer eliminationCount = Integer.parseInt(scanner.nextLine());
+			boolean inputCharIsValid = false;
+			while (!inputCharIsValid){
+				System.out.print("Enter circle size: ");
+				inputChar = scanner.nextLine();
+				if (!StringUtils.isNumeric(inputChar))
+					System.out.println("Circle size must be numeric, and greater than one");
+				else if(Integer.parseInt(inputChar) < 2)
+					System.out.println("Circle size must be greater than one");
+				else
+					inputCharIsValid = true;
+			}
+			Integer circleSize = Integer.parseInt(inputChar);
+
+			inputCharIsValid = false;
+			while (!inputCharIsValid){
+				System.out.print("Enter elimination count : ");
+				inputChar = scanner.nextLine();
+				if (!StringUtils.isNumeric(inputChar))
+					System.out.println("Elimination count must be numeric, and greater than zero");
+				else if(Integer.parseInt(inputChar) < 1)
+					System.out.println("Elimination count must be greater than zero");
+				else
+					inputCharIsValid = true;
+			}
+			Integer eliminationCount = Integer.parseInt(inputChar);
 			
 			System.out.println("Id of winning child: " + childService.getWinner(circleSize, eliminationCount));
 			
